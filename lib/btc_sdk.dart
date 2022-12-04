af08b1107a6906f866d2faf4ea484e9f5070a38f
Patrick Di Loreto <patrick@Patricks-MacBook-Pro.local>
2022-12-04 10:02:52 +0100

    exporting libraries

--- lib/btc_sdk.dart
+++ lib/btc_sdk.dart
@@ -1,8 +1,10 @@
-/// Support for doing something awesome.
+/// Utility library to work with btcs wallets and transaction types.
 ///
-/// More dartdocs go here.
+/// More dartdocs go here...
 library btc_sdk;
 
 export 'src/btc_sdk_base.dart';
+export 'src/model/binary/uint.dart';
+export 'src/model/binary/var_int.dart';
+export 'src/model/binary/buffer.dart';
 
-// TODO: Export any libraries intended for clients of this package.
