be4210bbd59dfa6548de917ac50124e953503c4a
Patrick Di Loreto <patrick@Patricks-MacBook-Pro.local>
2022-12-06 16:53:09 +0100

    adding pubkey test

---
+++ test/model/crypto/public_key_test.dart
@@ -0,0 +1,23 @@
+import 'package:btc_sdk/btc_sdk.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('public key', () {
+    test('creation', () {
+      PrivateKey prvKey = PrivateKey.parseHex('ef235aacf90d9f4aadd8c92e4b2562e1d9eb97f0df9ba3b508258739cb013db2');
+      PublicKey pubKey = prvKey.publicKey;
+
+      final hexX = 'b4632d08485ff1df2db55b9dafd23347d1c47a457072a1e87be26896549a8737';
+      final hexY = '8ec38ff91d43e8c2092ebda601780485263da089465619e0358a5c1be7ac91f4';
+      final expectedPoint = BigIntPoint(
+        x: hexX.toUint8ListFromHex!.toBigInt,
+        y: hexY.toUint8ListFromHex!.toBigInt
+      );
+
+      expect(pubKey.point, expectedPoint);
+      expect(pubKey.uncompressed.toHex, '04' + hexX + hexY);
+      expect(pubKey.compressed.toHex, '02' + hexX);
+      expect(pubKey.toPublicKeyHash, Hash.hash160(pubKey.compressed));
+    });
+  });
+}
